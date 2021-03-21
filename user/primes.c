2833c838404422fee62682ff0eff2a02a06694f7
xwj1427698750 <17352959682@163.com>
2021-03-21 22:58:29 +0800

    完善primes

--- user/primes.c
+++ user/primes.c
@@ -4,22 +4,25 @@
 
 int child_process(int p_left[]){
     int prime;
-    if(read(p_left[0],&prime,4)>0){
+    if(read(p_left[0],&prime,sizeof(prime))>0){//递归调用的边界
         printf("prime %d\n",prime);
+    }else{
+        exit(0);
     }
     int pid;
     int p_right[2];
     pipe(p_right);
     pid = fork();
     if(pid == 0){
+        close(p_left[0]);
         close(p_right[1]);//子进程只需要从管道中读取数据，关闭写端
         child_process(p_right);
     }else{
         int num;
         close(p_right[0]);//父进程只需要从管道中写数据，关闭读端
-        while(read(p_left[0],&num,4)>0){
+        while(read(p_left[0],&num,sizeof(num))>0){
             if(num%prime != 0){
-                write(p_right[1],&num,4);
+                write(p_right[1],&num,sizeof(num));
             }
         }
         close(p_left[0]);
@@ -31,8 +34,12 @@ int child_process(int p_left[]){
 }
 
 int
-main()
+main(int argc, char *argv[])
 {
+    if(argc>1){
+        fprintf(2, "usage: primes\n");
+        exit(1);
+    }
     int pid;
     int p[2];
     pipe(p);
@@ -44,7 +51,7 @@ main()
         close(p[0]);//父进程只需要从管道中写数据，关闭读端
         int i;
         for(i = 2;i<=35;i++){
-            write(p[1],&i,4);
+            write(p[1],&i,sizeof(i));
         }
         close(p[1]);
         while(wait((int*)0)!=-1)
