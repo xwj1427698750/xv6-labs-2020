39c784a4e07ad56e70ebce202a3b54c945496cc3
xwj1427698750 <17352959682@163.com>
2021-03-21 22:49:42 +0800

    完成sleep,pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,30 @@
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/fcntl.h"
+
+int
+main()
+{
+    int pid;
+    int p[2];
+    char buf[10];
+    pipe(p);
+    pid = fork();
+    if(pid == 0){
+        read(p[0],buf, sizeof(buf));
+        printf("%d: received ping\n",getpid());
+        close(p[0]);
+        write(p[1],"2", 1);
+        close(p[1]);
+    } else {
+        write(p[1],"1",1);
+        close(p[1]);
+        wait((int*)0);//这一句很关键，等待子进程从管道中读取数据，否则数据会被父进程中下一句的read给读走
+        read(p[0],buf, sizeof(buf));
+        close(p[0]);
+        printf("%d: received pong\n",getpid());
+    }
+
+    exit(0);
+}
+
---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    int time;
+
+    if(argc < 2){
+        fprintf(2, "Usage: sleep time...\n");
+        exit(1);
+    }
+    time = atoi(argv[1]);
+
+    sleep(time);
+
+    exit(0);
+}
+
